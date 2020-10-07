3e74505aa28d7969e71e57b2a6b709381c403cbd
Marius Andra <marius.andra@gmail.com>
2020-10-07 13:27:16 +0200

    geoip plugin

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# helloworldplugin
-Posthog hello world plugin
+# posthog-maxmind-plugin
+
+Enrich your collected events with GeoIP data from MaxMind
+
+1. Install [posthog-cli](https://github.com/PostHog/posthog-cli)
+2. Install this plugin `posthog plugin install https://github.com/mariusandra/posthog-maxmind-plugin`
+3. [Sign up](https://dev.maxmind.com/geoip/geoip2/geolite2/) to maxmind 
+4. Download the GeoLite2 City (or the paid GeoIP City) database
+5. Run posthog with the env `MAXMIND_GEOIP_DATABASE=/path/to/GeoLite2-City.mmdb`
+6. Optional: Set up [automatic updates](https://dev.maxmind.com/geoip/geoipupdate/#Direct_Downloads) as described here.
--- __init__.py
+++ __init__.py
@@ -1,13 +1,25 @@
-class Plugin(ABC):
+import os
+import geoip2.database
 
-    @abstractmethod
+geoip_path = os.environ.get("MAXMIND_GEOIP_DATABASE", None)
+
+if geoip_path:
+    reader = geoip2.database.Reader(geoip_path)
+else:
+    print("🔻 Running posthog-maxmind-plugin without MAXMIND_GEOIP_DATABASE")
+    print("🔺 No GeoIP data will be ingested!")
+    reader = None
+
+class MaxmindPlugin():
     def process_event(self, event):
-        pass
-
-    @abstractmethod
-    def process_person(self, event):
-        pass
-
-    @abstractmethod
-    def process_identify(self, event):
-        pass
+        if reader:
+            ip = event.properties.get('$ip', None)
+            if ip:
+                response = reader.city(ip)
+                event.properties['$country_iso'] = response.country.iso_code
+                event.properties['$country_name'] = response.country.name
+                event.properties['$region_iso'] = response.subdivisions.most_specific.iso_code
+                event.properties['$region_name'] = response.subdivisions.most_specific.name
+                event.properties['$city_name'] = response.city.name
+                event.properties['$latitude'] = response.location.latitude
+                event.properties['$longitude'] = response.location.longitude
