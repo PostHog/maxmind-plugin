092ce11e38cb7067e1ce1568e27ba2b14faabc22
Marius Andra <marius.andra@gmail.com>
2020-11-06 15:58:27 +0100

    new global config

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,55 @@
-import Reader, { CityResponse } from 'mmdb-lib'
-import { PluginEvent, PluginMeta } from 'posthog-plugins'
+import Reader, { CityResponse, Response } from 'mmdb-lib'
+import { PluginMeta, PluginEvent, PluginAttachment } from 'posthog-plugins'
 
-export function setupTeam({ config }) {
-    console.log("Setting up the team MAXMIND!")
-    console.log(config)
+interface Meta extends PluginMeta {
+    config: {
+        localhostIP: string
+    },
+    attachments: {
+        maxmindMmdb?: PluginAttachment
+    },
+    global: {
+        ipLookup?: Reader<Response>
+    }
 }
 
-export function processEvent(event: PluginEvent, meta: PluginMeta) {
-    if (event.ip && event.properties && meta.attachments['maxmindMmdb']) {
-        const lookup = new Reader(meta.attachments['maxmindMmdb'].contents);
-        const response = lookup.get(event.ip) as CityResponse
+export function setupPlugin({ attachments, global }: Meta) {
+    if (attachments.maxmindMmdb) {
+        global.ipLookup = new Reader(attachments.maxmindMmdb.contents)
+    }
+}
+
+export function processEvent(event: PluginEvent, { global, config }: Meta) {
+    if (event.ip === "127.0.0.1" && config.localhostIP) {
+        event.ip = config.localhostIP
+    }
+    if (event.ip && event.properties && global.ipLookup) {
+        const response = global.ipLookup.get(event.ip) as CityResponse
         if (response) {
             if (response.city) {
-                event.properties['city_id'] = response.city.geoname_id
-                event.properties['city_name'] = response.city.names?.en
+                event.properties['$city_id'] = response.city.geoname_id
+                event.properties['$city_name'] = response.city.names?.en
             }
             if (response.country) {
-                event.properties['country_name'] = response.country.names?.en
-                event.properties['country_code'] = response.country.iso_code
+                event.properties['$country_name'] = response.country.names?.en
+                event.properties['$country_code'] = response.country.iso_code
             }
             if (response.continent) {
-                event.properties['continent_name'] = response.continent.names?.en
-                event.properties['continent_code'] = response.continent.code
+                event.properties['$continent_name'] = response.continent.names?.en
+                event.properties['$continent_code'] = response.continent.code
             }
             if (response.postal) {
-                event.properties['postal_code'] = response.postal.code
+                event.properties['$postal_code'] = response.postal.code
             }
             if (response.location) {
-                event.properties['latitude'] = response.location?.latitude
-                event.properties['longitude'] = response.location?.longitude
-                event.properties['time_zone'] = response.location?.time_zone
+                event.properties['$latitude'] = response.location?.latitude
+                event.properties['$longitude'] = response.location?.longitude
+                event.properties['$time_zone'] = response.location?.time_zone
             }
             if (response.subdivisions) {
                 response.subdivisions.forEach((subDivision, index) => {
-                    event.properties[`subdivision_${index + 1}_code`] = subDivision.iso_code
-                    event.properties[`subdivision_${index + 1}_name`] = subDivision.names?.en
+                    event.properties[`$subdivision_${index + 1}_code`] = subDivision.iso_code
+                    event.properties[`$subdivision_${index + 1}_name`] = subDivision.names?.en
                 })
             }
         }
